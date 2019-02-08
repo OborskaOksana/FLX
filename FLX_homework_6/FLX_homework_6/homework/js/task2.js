a62521c6bca246f3d9fdbf3feeb94b9875e151ad
Oksana_Oborska <46045246+OborskaOksana@users.noreply.github.com>
2019-02-09 01:20:00 +0800

    Add files via upload

--- FLX_homework_6/FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/FLX_homework_6/homework/js/task2.js
@@ -1,16 +1,18 @@
-// Your code goes here
+var amount=parseInt(prompt("Enter 0>amount<=9999999", "amount"));
+var discount=parseInt(prompt("Enter 0>discount<=99", "discount"));
+var total=parseInt(total);
+var amount_discount=parseInt(amount_discount);
 
-var amount = prompt("Enter 0>amount<=9999999", "amount");
-var discount = prompt("Enter 0>discount<=99", "discount");
-var total, amount_discount;
-
-if (0 = amount <= 9999999 && 0 < discount <= 99) {
-  total = (amount * discount) / 100;
+if (0 < amount <= 9999999 && 0 < discount <= 99) {
+  total = (amount * discount)/100;
   amount_discount = (amount - total);
   total.toFixed(2);
   amount_discount.toFixed(2);
   alert(
-    "Price without discount: " + amount + "Discount: " + discount + "Price with discount: " + amount_discount + "Saved: " + total);
+    "Price without discount: " + amount +'\n'+ 
+    "Discount: " + discount + '\n'+
+    "Price with discount: " + amount_discount +'\n'+
+     "Saved: " + total);
 } else if (amount < 0 || discount < 0) {
   alert("invalid input data");
 }
